2b1c47549a0170047a30a695a0f87b02fb546394
renegomezjr <renegomezjr@gmail.com>
2016-01-11 16:23:59 -0500

    Rspec changes, first passing test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
 
   describe "GET show" do
+    before do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      user = User.create!(name: "User name", email: "email@email.com", password: "password")
+      sign_in user
+    end
+
     it "returns http success" do
       get :show
       expect(response).to have_http_status(:success)
