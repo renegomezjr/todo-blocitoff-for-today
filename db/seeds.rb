c5689f4e561e360a957f50749e29b73933321fa8
renegomezjr <renegomezjr@gmail.com>
2016-01-18 13:00:08 -0500

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+20.times do |n|
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: "password")
+end
+
+50.times do |n|
+  Item.create!(
+    user_id: Faker::Number.between(1,20),
+    name: Faker::Lorem.sentence)
+end
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Item.count} items created"
