6c47d79e8c7a8a11c8a133b558c7c5e25671fc43
renegomezjr <renegomezjr@gmail.com>
2016-01-20 18:28:55 -0500

    Mark to do items complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,16 +2,34 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     @user = User.find(params[:user_id])
-    @item.user = @user 
+    @item.user = @user
 
     if @item.save
       flash[:notice] = "Item was saved!"
       redirect_to @user
     else
-      flash.now[:alert] = "Error saving item."
+      flash[:alert] = "Error saving item."
       redirect_to @user
     end
   end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @item = @user.items.find(params[:id])
+
+    if @item.destroy
+      flash[:notice] = "You did it! You are the master of this list."
+    else
+      flash[:alert] = "Something went wrong. Try again."
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+
 
   private
   def item_params
